4c03c8f3b28e3ab41382f005c6ad32eb2ef135c0
Antoine Germanique <antoine.germanique@nrb.be>
2021-09-16 09:48:44 +0200

    local repo import

--- cmd/grafana-reporter/handler.go
+++ cmd/grafana-reporter/handler.go
@@ -27,8 +27,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/antoineNRB/reporter/grafana"
+	"github.com/antoineNRB/reporter/report"
 	"github.com/gorilla/mux"
 )
 
--- cmd/grafana-reporter/main.go
+++ cmd/grafana-reporter/main.go
@@ -22,8 +22,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/IzakMarais/reporter/grafana"
-	"github.com/IzakMarais/reporter/report"
+	"github.com/antoineNRB/reporter/grafana"
+	"github.com/antoineNRB/reporter/report"
 	"github.com/gorilla/mux"
 )
 
